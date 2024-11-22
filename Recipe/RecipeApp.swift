cbe7862aaf8368203bbc1c3d985378dd277ca84f
roman <kamensky.ra@gmail.com>
2024-11-22 12:43:59 +0100

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by roman on 22.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
